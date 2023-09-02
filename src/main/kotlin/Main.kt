451d824dd5bcd2838edc5267534f6fdf31a3957c
andronov <mikhail_andronov@mail.ru>
2023-09-02 15:39:39 +0300

    Removed older code of Gradle tutorial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,6 @@
-import com.gradle.tutorial.FizzBuzzProcessor
+
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    for (i in 1..100) {
-        println(FizzBuzzProcessor.convert(i))
-    }
 }
