44129149ec37952820cb9a89e899d607071ec465
andronov <mikhail_andronov@mail.ru>
2023-09-29 19:08:06 +0300

    Completed InMemoryFieldsRepository

--- src/main/kotlin/com/ma/rovers/repositories/InMemoryFieldsRepository.kt
+++ src/main/kotlin/com/ma/rovers/repositories/InMemoryFieldsRepository.kt
@@ -1,8 +1,6 @@
 package com.ma.rovers.repositories
 
-import com.ma.rovers.domain.Field
 import com.ma.rovers.domain.IField
-import com.ma.rovers.domain.IntSize
 
 class InMemoryFieldsRepository: IFieldsRepository {
     companion object {
@@ -14,8 +12,7 @@ class InMemoryFieldsRepository: IFieldsRepository {
 
     override fun restore(): IField {
         if (storedField == null) throw NoSuchElementException("There's no any field stored in database")
-        //return storedField as IField
-        return Field(IntSize(storedField!!.length.size), IntSize(storedField!!.width.size))
+        return storedField as IField
     }
 
     override fun delete() {
